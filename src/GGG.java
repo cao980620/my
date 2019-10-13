7a306331419fdcbe866de154848806d3d451da2a
cao980620 <cao980620@outlook.com>
2019-10-13 11:27:52 +0800

    bvbnm,

---
+++ src/GGG.java
@@ -0,0 +1,9 @@
+public class GGG {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello word");
+        System.out.println("/n");
+        System.out.println("your are famuli");
+        System.out.println("sh");
+    }
+}
